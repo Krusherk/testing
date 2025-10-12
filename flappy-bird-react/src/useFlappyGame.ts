901fc9d890e2f29312f942bae777bce07fbf48e2
Krusherk <89709403+Krusherk@users.noreply.github.com>
2025-10-12 16:19:50 +0100

    Update useFlappyGame.ts

--- flappy-bird-react/src/useFlappyGame.ts
+++ flappy-bird-react/src/useFlappyGame.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { ethers } from 'ethers';
-import { usePrivy, CrossAppAccountWithMetadata } from "@privy-io/react-auth";
+import { usePrivy, useWallets, CrossAppAccountWithMetadata } from "@privy-io/react-auth";
 
 type GameState = 'Start' | 'Ready' | 'Play' | 'End';
 
